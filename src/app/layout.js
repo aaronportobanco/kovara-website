9e5d7f62b28b3a799131d430cf0fa49a6d61fde8
Aarón Portobanco <aaronportobanco@gmail.com>
2025-04-16 20:10:23 -0600

    Refactor RootLayout component: update font import and enhance layout structure for improved readability

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import { Montserrat_Alternates } from "next/font/google";
+import NavBar from "@/components/layout/Navbar";
 import "./globals.css";
 
 export const metadata = {
@@ -6,12 +8,15 @@ export const metadata = {
   description: "Descubre lo ultimo en tecnología en nuestra tienda",
 };
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat_Alternates({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
