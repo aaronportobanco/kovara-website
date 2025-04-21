b9172acd383ae4cd8e35f3ba9759f425f80d45f5
Aarón Portobanco <aaronportobanco@gmail.com>
2025-04-20 19:30:18 -0600

    Add Breadcrumb component for improved navigation structure

---
+++ src/components/layout/BreadcrumbComp.jsx
@@ -0,0 +1,28 @@
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+  } from "@/components/ui/breadcrumb";
+  
+  const BreadcrumbComp = ({page}) => {
+    return (
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>{page}</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    );
+  };
+
+  export default BreadcrumbComp;
