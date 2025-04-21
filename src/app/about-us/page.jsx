1681b761b8cce235f46d82cd9144cae008f1ace1
Norwin-HD <norwinherrera01@gmail.com>
2025-04-21 12:24:54 -0600

    Actualiza contenido de la página About

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -11,6 +11,11 @@ const AboutUsPage = () => {
           <h2>Card Title</h2>
           <p>This is a card component.</p>
         </Card>
+
+        <Card>
+          <h2>Card Title</h2>
+          <p>This is a card component.</p>
+        </Card>
       
       <p>Hola Mundo</p>
     </div>
