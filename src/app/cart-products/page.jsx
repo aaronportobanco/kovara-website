adcaa5d3d8059ef8b189b4aec51200779f701ce6
Aarón Portobanco <aaronportobanco@gmail.com>
2025-04-22 22:41:29 -0600

    Refactor CartPage to utilize cart context for dynamic product listing and clear cart functionality

--- src/app/cart-products/page.jsx
+++ src/app/cart-products/page.jsx
@@ -3,11 +3,13 @@
 import BreadcrumbComp from "@/components/layout/BreadcrumbComp";
 import PageItem from "./components/PageItem";
 import Checkout from "./components/Checkout";
-import { DataProducts } from "@/data/MockData";
+import { useCart } from "./context/cart-context";
 import { Button } from "@/components/ui/button";
 import { Trash2 } from "lucide-react";
 
 const CartPage = () => {
+  const { cart, clearCart } = useCart(); // Obtener productos del carrito
+
   return (
     <section className="flex flex-col p-6 md:px-6 gap-8 text-foreground bg-background">
       {/* Breadcrumb y encabezado */}
@@ -25,21 +27,26 @@ const CartPage = () => {
         {/* Lista de productos */}
         <div className="flex flex-col gap-6 flex-1 w-full">
           <div className="flex items-center justify-between">
-            <h2 className="text-base font-bold">Productos (13)</h2>
-            <Button variant="outline" className="flex items-center gap-2">
+            <h2 className="text-base font-bold">
+              Productos ({cart.length})
+            </h2>
+            <Button
+              variant="outline"
+              className="flex items-center gap-2"
+              onClick={clearCart} // Vaciar carrito
+            >
               <Trash2 className="w-4 h-4" />
               Vaciar carrito
             </Button>
           </div>
-          {DataProducts.map((product) => (
+          {cart.map((product) => (
             <PageItem key={product.id} data={product} />
-            
           ))}
         </div>
 
         {/* Checkout (Resumen de compra) */}
         <div className="w-full lg:w-[350px] shrink-0">
-          <Checkout subtotal={1799.87} impuestos={316.8} total={3037.4} />
+          <Checkout />
         </div>
       </div>
     </section>
