15d955460e1c2e1019b07abcd59aaa05506a3174
Norwin-HD <norwinherrera01@gmail.com>
2025-04-23 18:12:42 -0600

    Refactor components for improved styling and layout consistency

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 // Custom Components
 import ManualMarca from './components/ViewPDF';
@@ -17,25 +18,30 @@ import Canva from "./components/CanvaSection";
 const AboutUsPage = () => {
   
   return (
-    <main className="flex flex-col items-center gap-6 px-8 py-5 bg-slate-900">
+    <main className="flex flex-col gap-6 px-8 py-5 bg-slate-900">
       <BreadcrumbComp page="Sobre nosotros" />
 
       {/* Encabezado */}
-      <section className="flex justify-between items-center w-full py-6">
+      <section className="flex flex-col md:flex-row justify-between items-start md:items-center w-full py-6 gap-6">
         <div className="flex flex-col gap-4 flex-1">
           <h1 className="text-white text-3xl font-bold">Sobre Nosotros</h1>
           <p className="text-slate-400 text-base font-medium">
             Conoce más sobre Kovara y nuestra misión como empresa
           </p>
         </div>
-        <div className="flex gap-5 flex-1 justify-end">
-          <Button>
-            <ArrowRight className="h-4 w-4 mr-2" />
-            Ver Productos
-          </Button>
-          <Button variant="outline">Contáctanos</Button>
+        <div className="flex justify-start md:justify-end flex-1">
+          <Link href="/products">
+            <Button className="group flex items-center">
+              Ver Productos
+              <ArrowRight
+                strokeWidth={2.5}
+                className="ml-2 transition-transform duration-300 group-hover:translate-x-1"
+              />
+            </Button>
+          </Link>
         </div>
       </section>
+
 
       {/* Contenido */}
       <section className="flex flex-col gap-20 w-full">
