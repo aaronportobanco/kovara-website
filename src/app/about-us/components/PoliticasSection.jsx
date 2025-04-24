dcf64a8843a64cd3d441a101d6f0f8be81a14a0f
Norwin-HD <norwinherrera01@gmail.com>
2025-04-23 18:50:11 -0600

    Refactor PoliticasSection component for improved readability and structure

--- src/app/about-us/components/PoliticasSection.jsx
+++ src/app/about-us/components/PoliticasSection.jsx
@@ -8,11 +8,12 @@ import {
   HandCoins, ShieldCheck, EarthLock
 } from "lucide-react";
 
+// Comentario agregado para forzar commit XD
 
 const Politica = () => {
     return (
-            <Section title="Políticas Internas">
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <Section title="Políticas Internas">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 <CardItem icon={<HandCoins />} title={"Metodos de pago"}>
                     Se aceptan pagos en efectivo, transferencias bancarias y tarjetas de crédito/débito.
                 </CardItem>
@@ -25,30 +26,30 @@ const Politica = () => {
                 <CardItem icon={<EarthLock />} title={"Privacidad"}>
                     La información del cliente será tratada con total confidencialidad.
                 </CardItem>
-                </div>
-            </Section>
-        )
+            </div>
+        </Section>
+    )
 }
 
 const Section = ({ title, children }) => (
     <section className="flex flex-col gap-6 w-full">
-      <h2 className="text-white text-2xl font-bold border-b border-slate-400 pb-2">
-        {title}
-      </h2>
-      {children}
+        <h2 className="text-white text-2xl font-bold border-b border-slate-400 pb-2">
+            {title}
+        </h2>
+        {children}
     </section>
-  );
-  
-  const CardItem = ({ icon, title, children }) => (
+);
+
+const CardItem = ({ icon, title, children }) => (
     <Card className="bg-slate-800 text-white border border-slate-600">
-      <CardContent className="p-5 space-y-4">
-        <div className="flex items-center gap-3">
-          <div className="bg-slate-600 p-2 rounded-full">{icon}</div>
-          <h3 className="text-lg font-semibold">{title}</h3>
-        </div>
-        <p className="text-sm text-slate-300">{children}</p>
-      </CardContent>
+        <CardContent className="p-5 space-y-4">
+            <div className="flex items-center gap-3">
+                <div className="bg-slate-600 p-2 rounded-full">{icon}</div>
+                <h3 className="text-lg font-semibold">{title}</h3>
+            </div>
+            <p className="text-sm text-slate-300">{children}</p>
+        </CardContent>
     </Card>
-  );
+);
 
 export default Politica;
