1da1e9a4db4446aa4cbd3b9ce4284cc0e75dc4cb
Aarón Portobanco <aaronportobanco@gmail.com>
2025-06-10 16:47:46 -0600

    refactor: adjust layout and spacing in CheckOut component for improved readability

--- src/app/checkout-page/page.jsx
+++ src/app/checkout-page/page.jsx
@@ -8,15 +8,15 @@ import BreadcrumbComp from "@/components/layout/BreadcrumbComp";
 import TransactionSummary from "./components/TransactionSummary";
 
 const CheckOut = () => {
-// Manage the current step in the checkout process
-const [step, setStep] = React.useState(1); // 1: shipping, 2: payment, 3: summary
+  // Manage the current step in the checkout process
+  const [step, setStep] = React.useState(1); // 1: shipping, 2: payment, 3: summary
 
-// Function to handle step change
-const handleNext = () => setStep((prev)=> prev + 1);
-const handleBack = () => setStep((prev)=> prev - 1);
+  // Function to handle step change
+  const handleNext = () => setStep((prev) => prev + 1);
+  const handleBack = () => setStep((prev) => prev - 1);
 
   return (
-    <section id="checkout" className="flex flex-col p-4 md:px-6 gap-8">
+    <section id="checkout" className="flex flex-col p-4 md:px-6 gap-9">
       {/* Breadcrumb y encabezado */}
       <BreadcrumbComp page="Checkout" />
 
@@ -26,16 +26,19 @@ const handleBack = () => setStep((prev)=> prev - 1);
           Complete su compra de forma segura.
         </p>
       </div>
-      <div className="grid gap-8 lg:grid-cols-3 rounded-2xl pt-2">
-        <div className="lg:col-span-2">
-          {step === 1 && <ShippingForm onNext={handleNext} />}
-          {step === 2 && <PaymentForm onNext={handleNext} onBack={handleBack} />}
-          {step === 3 && <TransactionSummary onBack={handleBack} />}
+      {step === 3 ? (
+        <TransactionSummary onBack={handleBack} onNext={handleNext}/>
+      ) : (
+        <div className="grid gap-8 lg:grid-cols-3">
+          <div className="lg:col-span-2">
+            {step === 1 && <ShippingForm onNext={handleNext} />}
+            {step === 2 && <PaymentForm onNext={handleNext} onBack={handleBack} />}
+          </div>
+          <div className="lg:col-span-1 rounded-2xl">
+            <OrderSummary step={step} />
+          </div>
         </div>
-        <div className="lg:col-span-1 rounded-2xl">
-          <OrderSummary step={step} />
-        </div>
-      </div>
+      )}
     </section>
   );
 };
